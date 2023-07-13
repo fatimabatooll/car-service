caa1f8d23d6889544ecb9326d7b4a05c89b8e2f5
fatimabatooll <fatima.batool@xloopdigital.com>
2023-07-13 10:32:13 +0500

    first commit

---
+++ src/test/java/com/glc/car/CarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glc.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
